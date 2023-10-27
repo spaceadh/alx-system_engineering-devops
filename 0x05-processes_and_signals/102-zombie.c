a0771834885344d06afa2ace96f3df70aaf20696
curry-bean <curry.bean.017@gmail.com>
2023-10-27 10:04:57 +0300

    I swearHawatakiNicheck

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -10,14 +10,13 @@ int main(void)
     for (i = 0; i < 5; i++) {
         child_pid = fork();
         if (child_pid == 0) {
-            // Child process exits immediately, becoming a zombie
+            
             exit(0);
         } else if (child_pid > 0) {
             printf("Zombie process created, PID: %d\n", child_pid);
         }
     }
 
-    // Infinite loop to keep the program running
     while (1) {
         sleep(1);
     }
