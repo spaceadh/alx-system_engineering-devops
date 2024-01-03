9625134e21c742faa64f105a74de983c6330fcbb
spaceadh <spacesealadhesives13@gmail.com>
2024-01-03 13:44:18 +0300

    Changes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,24 @@
-# Setup nginx server
+# Script to install nginx using puppet
 
-package { 'nginx':
-  ensure     => 'installed',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-file_line { 'aaaaa':
+package {'nginx':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+exec {'Hello World!':
+  command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
